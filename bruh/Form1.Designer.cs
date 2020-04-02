f3673a55432595cd3dd44d8aeff6a4c0b4e0c1d2
Adele Gryba <adelgryb243@ed.amdsb.ca>
2020-04-02 13:06:30 -0400

    uh

---
+++ bruh/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace bruh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(323, 167);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(107, 108);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "lets go!";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Location = new System.Drawing.Point(254, 302);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(283, 93);
+            this.outputLabel.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(330, 112);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ bruh/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bruh
+{
+    public partial class Form1 : Form
+    {
+        string chefName;
+        int scene;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            chefName = textBox1.Text;
+            scene = 1;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.M)
+            {
+                outputLabel.Text = "you chose left or whatever";
+            }
+            else
+            {
+                outputLabel.Text = "this ones right";
+            }
+
+            //ignore this its just for each scene still figuring it out
+            switch (scene)
+            {
+                case 0:
+                    //first scene, choose your path
+                    outputLabel.Text = "welcome chef " + chefName + " !!";
+                    outputLabel.Text = "\n this kitchen can get pretty crazy, so show your skills, try to keep up and you might get the job!";
+                    outputLabel.Text = "\n first of all,would you like formal dining or casual dining?";
+                    //if M key formal, if C key casual 
+                    if (e.KeyCode == Keys.M)
+                    {
+                        outputLabel.Text = "formal";
+                        scene = 1;
+                    }
+                    if (e.KeyCode == Keys.C)
+                    {
+                        outputLabel.Text = "casual";
+                        scene = 2;
+                    }
+                    startButton.Visible = false;
+                    textBox1.Visible = false;
+                    break;
+                case 1:
+                   
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                case 10:
+                    break;
+                case 11:
+                    break;
+                case 12:
+                    break;
+                case 13:
+                    break;
+                case 14:
+                    break;
+                case 15:
+                    break;
+                case 16:
+                    break;
+                case 17:
+                    break;
+                case 18:
+                    break;
+                case 19:
+                    break;
+                case 20:
+                    break;
+                case 21:
+                    break;
+                case 22:
+                    break;
+                case 23:
+                    break;
+                case 24:
+                    break;
+                case 25:
+                    break;
+                case 26:
+                    break;
+                case 27:
+                    break;
+                case 28:
+                    break;
+                case 29:
+                    break;
+            }
+        }
+    }
+}
